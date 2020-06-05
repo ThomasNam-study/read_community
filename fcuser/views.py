0192b1c60d31b98cffedbc62d5142e2ca8a92147
RED <purred@naver.com>
2020-06-05 13:15:34 +0900

    회원 가입 로직 작업

--- fcuser/views.py
+++ fcuser/views.py
@@ -1,11 +1,31 @@
+from django.contrib.auth.hashers import make_password
+from django.http import HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
+from fcuser.models import Fcuser
+
 
 def register(request):
-
     if request.method == 'GET':
         return render(request, 'register.html')
     elif request.method == 'POST':
-        return render(request, 'register.html')
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re-password', None)
+
+        res_data = {}
+
+        if not (username and password and re_password):
+            res_data['error'] = '모든 값을 입력해야 됩니다.'
+        elif password != re_password:
+            res_data['error'] = '비밀번호가 다릅니다.'
+        else:
+            fcuser = Fcuser(
+                username=username,
+                password=make_password(password)
+            )
+
+            fcuser.save()
+
+        return render(request, 'register.html', res_data)
