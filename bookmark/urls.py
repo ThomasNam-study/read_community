7da853c6ab86a9e5e426c83e6f9bad743e61cfe2
RED <purred@naver.com>
2020-06-08 10:06:56 +0900

    Bookmark 작업중

---
+++ bookmark/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from bookmark.views import BookmarkLv, BookmarkDv
+
+urlpatterns = [
+    path('/', BookmarkLv.as_view(), name='index'),
+    path('/<int:pk>/$', BookmarkDv.as_view(), name='detail'),
+]
--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+
+from bookmark.models import Bookmark
+
+
+class BookmarkLv(ListView):
+    model = Bookmark
+
+
+class BookmarkDv(DetailView):
+    model = Bookmark
