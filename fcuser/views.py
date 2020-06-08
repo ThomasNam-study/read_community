5262b5bfe5055e9183eff54e862e0475a6ab8a74
RED <purred@naver.com>
2020-06-08 12:52:23 +0900

    E-mail 필드 추가

--- fcuser/views.py
+++ fcuser/views.py
@@ -12,17 +12,19 @@ def register(request):
     elif request.method == 'POST':
         username = request.POST.get('username', None)
         password = request.POST.get('password', None)
+        useremail = request.POST.get('useremail', None)
         re_password = request.POST.get('re-password', None)
 
         res_data = {}
 
-        if not (username and password and re_password):
+        if not (username and password and re_password and useremail):
             res_data['error'] = '모든 값을 입력해야 됩니다.'
         elif password != re_password:
             res_data['error'] = '비밀번호가 다릅니다.'
         else:
             fcuser = Fcuser(
                 username=username,
+                useremail=useremail,
                 password=make_password(password)
             )
 
