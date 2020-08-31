08aeb32db6e11e4af594a6337715aa0544dc1221
RED <purred@naver.com>
2020-08-31 09:09:24 +0900

    프로젝트 상세 페이지 작업

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from product.views import ProductList, ProductCreate
+from product.views import ProductList, ProductCreate, ProductDetail
 
 urlpatterns = [
     path('', ProductList.as_view()),
+    path('<int:pk>/', ProductDetail.as_view()),
     path('create/', ProductCreate.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView, FormView
+from django.views.generic import ListView, FormView, DetailView
 
 from product.forms import RegisterForm
 from product.models import Product
@@ -16,3 +16,11 @@ class ProductCreate(FormView):
     template_name = 'register_product.html'
     form_class = RegisterForm
     success_url = '/product/'
+
+
+class ProductDetail(DetailView):
+    template_name = 'product_detail.html'
+
+    queryset = Product.objects.all()
+    context_object_name = 'product'
+
